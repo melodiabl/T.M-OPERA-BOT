9bf65d27305b6b59ae797db6edd07807fc561a71
melodiabl <melodiayaoivv@gmail.com>
2026-01-14 00:59:55 -0300

    Actualizar bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,10 @@ const { Collection } = require('discord.js')
 const Bot = require('./structures/Client.js')
 const client = new Bot()
 
-require('./handlers/events.js')(client)
-require('./handlers/commands.js')(client)
 client.commands = new Collection()
 client.cooldowns = new Collection()
+
+require('./handlers/events.js')(client)
+require('./handlers/commands.js')(client)
 
 client.login()
